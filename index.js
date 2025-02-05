a9106a946afac7b55c0bce485fe0cfc38495bd51
rioalifian_12 <rioalifian12@gmail.com>
2025-02-05 15:42:40 +0700

    update dan refactor code

--- index.js
+++ index.js
@@ -2,44 +2,64 @@ const readline = require("readline");
 const { stdin: input, stdout: output } = require("process");
 const validator = require("validator");
 const fs = require("fs");
-const { addAbortSignal } = require("stream");
-const { log } = require("console");
 
 const rl = readline.createInterface({ input, output });
 
-rl.question("What is your name? ", (name) => {
-  rl.question("What is your phone number? ", (phone) => {
-    rl.question("What is your email? ", (email) => {
-      const data = { name, phone, email };
+const dirPath = "./data";
+const dataPath = "./data/contacts.json";
 
-      console.log(`Your name is: ${name}`);
+if (!fs.existsSync(dirPath)) {
+  fs.mkdirSync(dirPath);
+}
 
-      const valPhone = validator.isMobilePhone(phone, ["id-ID"]);
-      const valEmail = validator.isEmail(email);
+if (!fs.existsSync(dataPath)) {
+  fs.writeFileSync(dataPath, "[]", "utf-8");
+}
 
-      valPhone
-        ? console.log(`Your phone number is: ${phone}`)
-        : console.log("Harus phone number dan tidak kurang dari 12");
-
-      valEmail
-        ? console.log(`Your email is: ${email}`)
-        : console.log("Format email salah!");
-
-      if (fs.existsSync("data/contacts.json")) {
-        const file = fs.readFileSync("data/contacts.json", "utf-8");
-        const contacts = JSON.parse(file);
-        !valPhone || !valEmail
-          ? console.log("data yang dimasukkan salah ulangi lagi !")
-          : contacts.push(data);
-        fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
-      } else {
-        fs.mkdir("./data", { recursive: true }, (err) => {
-          if (err) throw err;
-          fs.writeFileSync("data/contacts.json", "[]");
-        });
-      }
-
-      rl.close();
+// function untuk menanyakan pertanyaan kepada user dan mengembalikan jawabannya sebagai promise
+const question = async (question) => {
+  return new Promise((resolve, reject) => {
+    rl.question(question, (answer) => {
+      resolve(answer);
     });
   });
-});
+};
+
+// function untuk menyimpan contacts data kedalam file json
+const saveData = async (data) => {
+  const file = fs.readFileSync(dataPath, "utf-8");
+  const contacts = JSON.parse(file);
+  contacts.push(data);
+  fs.writeFileSync(dataPath, JSON.stringify(contacts));
+};
+
+// function untuk mengatur alur program
+const main = async () => {
+  const name = await question("What is your name? ");
+
+  let phone;
+  while (true) {
+    phone = await question("What is your phone number? ");
+    if (validator.isMobilePhone(phone, ["id-ID"])) {
+      break;
+    } else {
+      console.log("Invalid phone number, please try again!");
+    }
+  }
+
+  let email;
+  while (true) {
+    email = await question("What is your email? ");
+    if (validator.isEmail(email)) {
+      break;
+    } else {
+      console.log("Invalid email, please try again!");
+    }
+  }
+
+  const data = { name, phone, email };
+  saveData(data);
+  rl.close();
+};
+
+main();
