542e1ca38e2ddaae92df30f7083b29707e92c0d2
rioalifian_12 <rioalifian12@gmail.com>
2025-02-13 13:01:10 +0700

    Mebuat web sederhana menggunakan template engine ejs dan express ejs layouts

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const express = require("express");
+var expressLayouts = require("express-ejs-layouts");
+
 const app = express();
 const port = 3000;
 
-const render = require("./src/renderHTML");
+// template engine ejs. File html diubah ekstensinya menjadi .ejs
+app.set("view engine", "ejs");
+
+app.use(expressLayouts);
 
 app.get("/", (req, res) => {
-  render.renderHTML("index.html", res);
+  res.render("index", { nama: "Rio AS", title: "Home Page" });
 });
+
 app.get("/about", (req, res) => {
-  render.renderHTML("about.html", res);
+  res.render("about", { title: "About Page" });
 });
+
 app.get("/contact", (req, res) => {
-  render.renderHTML("contact.html", res);
+  cont = [
+    { name: "Rio", email: "rio@gmail.com" },
+    { name: "Ali", email: "ali@gmail.com" },
+    { name: "Fian", email: "fian@gmail.com" },
+    { name: "Santoso", email: "santoso@gmail.com" },
+  ];
+  res.render("contact", { cont, title: "Contact Page" });
+});
+
+app.get("/product/:idProduct", (req, res) => {
+  res.send(
+    `Product Page with ID: ${req.params.idProduct} <br> Category ID: ${req.query.idCategory}`
+  );
 });
 
 app.use("/", (req, res) => {
