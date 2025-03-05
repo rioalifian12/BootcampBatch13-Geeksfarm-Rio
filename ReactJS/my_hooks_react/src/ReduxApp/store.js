175b24766ba60efffffa68aa20b729071f3db9a6
rioalifian_12 <rioalifian12@gmail.com>
2025-03-05 09:51:51 +0700

    Menambahkan library redux-devtools/extension

--- ReactJS/my_hooks_react/src/ReduxApp/store.js
+++ ReactJS/my_hooks_react/src/ReduxApp/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "./counterSlice";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
 export default configureStore({
   reducer: {
     counter: counterSlice,
+    composeWithDevTools,
   },
 });
