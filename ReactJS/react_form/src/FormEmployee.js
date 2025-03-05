535bd946d2a0d589db8bf4af5d5567a28e060dbc
rioalifian_12 <rioalifian12@gmail.com>
2025-03-05 13:44:57 +0700

    Menambahkan style pada form

--- ReactJS/react_form/src/FormEmployee.js
+++ ReactJS/react_form/src/FormEmployee.js
@@ -11,144 +11,181 @@ const FormEmployee = () => {
   };
 
   return (
-    <div className="container mt-3 w-50">
+    <div className="container mt-3 w-25">
       <h1 className="text-center">Employee Form</h1>
       <div className="card">
         <div className="card-body m-3">
           <div className="mb-3">
             <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="d-flex mb-3 gap-5">
-                <label>First Name</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  {...register("firstName")}
-                />
-              </div>
-              <div className="d-flex mb-3 gap-5">
-                <label>First Name</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  {...register("lastName")}
-                />
-              </div>
-              <div className="d-flex mb-3 gap-5">
-                <label>Employed</label>
-                <input
-                  type="checkbox"
-                  value="isEmployed"
-                  {...register("employed")}
-                />
-              </div>
-
-              <div className="d-flex mb-3 gap-5">
-                <label>Education</label>
-                <select className="form-select" {...register("education")}>
-                  <option value="sd">SD</option>
-                  <option value="smp">SMP</option>
-                  <option value="sma">SMA</option>
-                  <option value="s1">S1</option>
-                  <option value="s2">S2</option>
-                  <option value="s3">S3</option>
-                </select>
-              </div>
-
-              <div className="d-flex mb-3 gap-5">
-                <label>Expertise</label>
-                <input
-                  type="checkbox"
-                  name="html"
-                  value="html"
-                  {...register("expertise")}
-                />
-                <label>HTML</label>
-                <input
-                  type="checkbox"
-                  name="css"
-                  value="css"
-                  {...register("expertise")}
-                />
-                <label>CSS</label>
-
-                <input
-                  type="checkbox"
-                  name="javascript"
-                  value="javascript"
-                  {...register("expertise")}
-                />
-                <label>Javascript</label>
-
-                <input
-                  type="checkbox"
-                  name="nodejs"
-                  value="nodejs"
-                  {...register("expertise")}
-                />
-                <label>NodeJS</label>
-
-                <input
-                  type="checkbox"
-                  name="reactjs"
-                  value="reactjs"
-                  {...register("expertise")}
-                />
-                <label>ReactJS</label>
-              </div>
-
-              <div className="d-flex mb-3 gap-5">
-                <label>Prefered Technology</label>
-                <div className="form-check">
+              <div className="mb-3 row">
+                <label className="col-sm-4 col-form-label">First Name</label>
+                <div className="col-sm-8">
                   <input
-                    className="form-check-input"
-                    type="radio"
-                    value="frontend"
-                    {...register("technology")}
+                    type="text"
+                    className="form-control"
+                    {...register("firstName")}
                   />
-                  <label>Frontend</label>
-                </div>
-                <div className="form-check">
-                  <input
-                    className="form-check-input"
-                    type="radio"
-                    value="backend"
-                    {...register("technology")}
-                  />
-                  <label>Backend</label>
-                </div>
-                <div className="form-check">
-                  <input
-                    className="form-check-input"
-                    type="radio"
-                    value="fullstack"
-                    {...register("technology")}
-                  />
-                  <label>Fullstack</label>
                 </div>
               </div>
 
-              <div className="d-flex mb-3 gap-5">
-                <label>Notes</label>
-                <textarea
-                  className="form-control"
-                  {...register("notes")}
-                ></textarea>
+              <div className="mb-3 row">
+                <label className="col-sm-4 col-form-label">Last Name</label>
+                <div className="col-sm-8">
+                  <input
+                    type="text"
+                    className="form-control"
+                    {...register("lastName")}
+                  />
+                </div>
               </div>
 
-              <div className="d-flex mb-3 gap-3 justify-content-center">
-                <input
-                  type="submit"
-                  className="btn btn-primary mt-3"
-                  value="Submit"
-                />
-                <input
-                  type="reset"
-                  className="btn btn-secondary mt-3"
-                  value="Reset"
-                />
+              <div className="mb-3 row">
+                <label className="col-sm-4 col-form-label">Employed</label>
+                <div className="col-sm-8 d-flex align-items-center">
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    {...register("employed")}
+                  />
+                </div>
               </div>
-              <div className="w-100 bg-secondary-subtle">
-                <p>{JSON.stringify(employee)}</p>
+
+              <div className="mb-3 row">
+                <label className="col-sm-4 col-form-label">Education</label>
+                <div className="col-sm-8">
+                  <select className="form-select" {...register("education")}>
+                    <option>-- Select --</option>
+                    <option value="sd">SD</option>
+                    <option value="smp">SMP</option>
+                    <option value="sma">SMA</option>
+                    <option value="s1">S1</option>
+                    <option value="s2">S2</option>
+                    <option value="s3">S3</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className="mb-3 row">
+                <label className="col-sm-4 col-form-label pt-0">
+                  Expertise
+                </label>
+                <div className="col-sm-8">
+                  <div className="form-check">
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      name="html"
+                      value="html"
+                      {...register("expertise")}
+                    />
+                    <label className="form-check-label">HTML</label>
+                  </div>
+                  <div className="form-check">
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      name="css"
+                      value="css"
+                      {...register("expertise")}
+                    />
+                    <label className="form-check-label">CSS</label>
+                  </div>
+                  <div className="form-check">
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      name="javascript"
+                      value="javascript"
+                      {...register("expertise")}
+                    />
+                    <label className="form-check-label">Javascript</label>
+                  </div>
+                  <div className="form-check">
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      name="nodejs"
+                      value="nodejs"
+                      {...register("expertise")}
+                    />
+                    <label className="form-check-label">NodeJS</label>
+                  </div>
+                  <div className="form-check">
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      name="reactjs"
+                      value="reactjs"
+                      {...register("expertise")}
+                    />
+                    <label className="form-check-label">ReactJS</label>
+                  </div>
+                </div>
+              </div>
+
+              <div className="mb-3 row">
+                <label className="col-sm-4 col-form-label pt-0">
+                  Prefered Technology
+                </label>
+                <div className="col-sm-8">
+                  <div className="form-check">
+                    <input
+                      className="form-check-input"
+                      type="radio"
+                      value="frontend"
+                      {...register("technology")}
+                    />
+                    <label className="form-check-label">Frontend</label>
+                  </div>
+                  <div className="form-check">
+                    <input
+                      className="form-check-input"
+                      type="radio"
+                      value="backend"
+                      {...register("technology")}
+                    />
+                    <label className="form-check-label">Backend</label>
+                  </div>
+                  <div className="form-check">
+                    <input
+                      className="form-check-input"
+                      type="radio"
+                      value="fullstack"
+                      {...register("technology")}
+                    />
+                    <label className="form-check-label">Fullstack</label>
+                  </div>
+                </div>
+              </div>
+
+              <div className="mb-3 row">
+                <label className="col-sm-4 col-form-label pt-0">Notes</label>
+                <div className="col-sm-8">
+                  <textarea
+                    className="form-control"
+                    {...register("notes")}
+                  ></textarea>
+                </div>
+              </div>
+
+              <div className="mb-3 row">
+                <div className="col-sm-12 text-center">
+                  <input
+                    type="submit"
+                    className="btn btn-primary me-2"
+                    value="Submit"
+                  />
+                  <input
+                    type="reset"
+                    className="btn btn-secondary"
+                    value="Reset"
+                  />
+                </div>
+              </div>
+              <div className="mb-3 row">
+                <div className="w-100 bg-secondary-subtle p-4">
+                  <pre>{JSON.stringify(employee, null, 2)}</pre>
+                </div>
               </div>
             </form>
           </div>
